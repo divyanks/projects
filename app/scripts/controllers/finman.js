2660bd21e76b2860bf225f15b96cf24310007d71
divyanks <divyank_s@yahoo.com>
2016-08-07 11:27:31 +0530

    cbp
    Cloud brokerage portal

---
+++ app/scripts/controllers/finman.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name projectsApp.controller:FinmanCtrl
+ * @description
+ * # FinmanCtrl
+ * Controller of the projectsApp
+ */
+angular.module('projectsApp')
+  .controller('FinmanCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
